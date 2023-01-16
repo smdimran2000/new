a1d8db6e5122af9f9818f67e4e03aca29753e3f8
my <my@AIWS-PC>
2023-01-16 20:04:09 +0530

    ok

---
+++ spring-java/src/test/java/com/simple/springjava/api/SpringJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simple.springjava.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
